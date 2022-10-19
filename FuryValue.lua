243f274b103c1b8deed98d0629ebf3261850f77b
hedi.benaba <hedi.benaba@fidesio.com>
2022-10-19 21:03:00 +0200

    Initial commit

---
+++ FuryValue.lua
@@ -0,0 +1,119 @@
+local function gearscore_getStat(stats, statName)
+    return stats[statName] or 0;
+end
+
+local function EnumerateTooltipLines_helper(...)
+    local result = {};
+    for i = 1, select('#', ...) do
+        local region = select(i, ...);
+        if region and region:GetObjectType() == 'FontString' then
+            local text = region:GetText();
+            if text ~= nil then
+                table.insert(result, text);
+            end
+        end
+    end
+    return result;
+end
+
+local function gearscore_program(tooltip)
+    local _, itemLink = tooltip:GetItem()
+    if itemLink and itemlink ~= '' then
+        local _, _, _, _, _, itemType, _, _, itemEquipLoc = GetItemInfo(itemLink);
+        if itemEquipLoc ~= '' and (itemType == 'Armure' or itemType == 'Arme') then
+            local stats = GetItemStats(itemLink);
+            local spellName = GetItemSpell(itemLink);
+            local arpen = gearscore_getStat(stats, 'ITEM_MOD_ARMOR_PENETRATION_RATING_SHORT');
+            local pa = gearscore_getStat(stats, 'ITEM_MOD_ATTACK_POWER_SHORT');
+            local agi = gearscore_getStat(stats, 'ITEM_MOD_AGILITY_SHORT');
+            local armure = gearscore_getStat(stats, 'RESISTANCE0_NAME');
+            local expertise = gearscore_getStat(stats, 'ITEM_MOD_EXPERTISE_RATING_SHORT');
+            local toucher = gearscore_getStat(stats, 'ITEM_MOD_HIT_RATING_SHORT');
+            local hate = gearscore_getStat(stats, 'ITEM_MOD_HASTE_RATING_SHORT');
+            local force = gearscore_getStat(stats, 'ITEM_MOD_STRENGTH_SHORT');
+            local crit = gearscore_getStat(stats, 'ITEM_MOD_CRIT_RATING_SHORT');
+            local gearscore = arpen + expertise + toucher + pa / 1.8 + agi / 0.815 + armure / 64.8 + hate / 2 + force / 0.69 + crit / 0.666;
+            local red = gearscore_getStat(stats, 'EMPTY_SOCKET_RED');
+            local yellow = gearscore_getStat(stats, 'EMPTY_SOCKET_YELLOW');
+            local blue = gearscore_getStat(stats, 'EMPTY_SOCKET_BLUE');
+            local gs_chasse_full_force = (red * 20 + yellow * 20 + blue * 20) / 0.69;
+            local gs_bonus_de_chasse = 0;
+            local tooltipTxt = EnumerateTooltipLines_helper(tooltip:GetRegions());
+            for key, text in pairs(tooltipTxt) do
+                local indexStart, indexEnd = string.find(text, _G['ITEM_SOCKET_BONUS']:sub(1, -3));
+                if indexStart ~= nil then
+                    local bonusString = string.sub(text, indexEnd + 2);
+                    local searchForce = string.find(bonusString, ' Force');
+                    local searchAgi = string.find(bonusString, ' Agilité');
+                    local searchPa = string.find(bonusString, ' à la puissance d\'attaque');
+                    local searchToucher = string.find(bonusString, ' au score de toucher');
+                    local searchExpertise = string.find(bonusString, ' au score d\'expertise');
+                    local searchCritique = string.find(bonusString, ' au score de coup critique');
+                    local searchHate = string.find(bonusString, ' au score de hâte');
+                    local searchArpen = string.find(bonusString, ' au score de pénétration d\'armure');
+                    if searchForce ~= nil then
+                        local valueForce = tonumber(string.sub(bonusString, 1, searchForce));
+                        gs_bonus_de_chasse = valueForce / 0.69;
+                    elseif searchAgi ~= nil then
+                        local valueAgi = tonumber(string.sub(bonusString, 1, searchAgi));
+                        gs_bonus_de_chasse = valueAgi / 0.815;
+                    elseif searchHate ~= nil then
+                        local valueHate = tonumber(string.sub(bonusString, 1, searchHate));
+                        gs_bonus_de_chasse = valueHate / 2;
+                    elseif searchPa ~= nil then
+                        local valuePa = tonumber(string.sub(bonusString, 1, searchPa));
+                        gs_bonus_de_chasse = valuePa / 1.8;
+                    elseif searchToucher ~= nil then
+                        local valueToucher = tonumber(string.sub(bonusString, 1, searchToucher));
+                        gs_bonus_de_chasse = valueToucher;
+                    elseif searchExpertise ~= nil then
+                        local valueExpertise = tonumber(string.sub(bonusString, 1, searchExpertise));
+                        gs_bonus_de_chasse = valueExpertise;
+                    elseif searchCritique ~= nil then
+                        local valueCritique = tonumber(string.sub(bonusString, 1, searchCritique));
+                        gs_bonus_de_chasse = valueCritique / 0.666;
+                    elseif searchArpen ~= nil then
+                        local valueArpen = tonumber(string.sub(bonusString, 1, searchArpen));
+                        gs_bonus_de_chasse = valueArpen;
+                    end
+                end
+            end
+            local gs_chasse_respectant_couleurs = red * 20 / 0.69 + yellow * 10 / 0.69 + yellow * 10 / 0.666 + blue * 10 / 0.69 + gs_bonus_de_chasse;
+            if gs_chasse_respectant_couleurs > gs_chasse_full_force then
+                gearscore = gearscore + gs_chasse_respectant_couleurs;
+            else
+                gearscore = gearscore + gs_chasse_full_force;
+            end
+            if itemEquipLoc == 'INVTYPE_HEAD' then
+                local meta = gearscore_getStat(stats, 'EMPTY_SOCKET_META');
+                if meta > 0 then
+                    tooltip:AddLine('Value: ' .. gearscore .. ' (sans compter la meta)', 1, 0.4, 1);
+                else
+                    tooltip:AddLine('Value: ' .. gearscore .. ' (mais pas de meta :/)', 1, 0.4, 1);
+                end
+            else
+                tooltip:AddLine('Value: ' .. gearscore, 1, 0.4, 1);
+            end
+            if gs_chasse_respectant_couleurs > gs_chasse_full_force then
+                tooltip:AddLine('A gemmer en respectant les couleurs.', 1, 0.4, 1);
+            elseif red > 0 or blue > 0 or yellow > 0 then
+                tooltip:AddLine('A gemmer full force.', 1, 0.4, 1);
+            end
+            if itemType == 'Arme' then
+                tooltip:AddLine('Ne prend pas en compte les dégâts/dps/vitesse de l\'arme.', 1, 0.4, 1);
+            end
+            if spellName ~= nil then
+                tooltip:AddLine('Ne prend pas en compte le "utiliser".', 1, 0.4, 1);
+            end
+        end
+    end
+end
+
+if gearscore_init == nil then
+    gearscore_init = true;
+    GameTooltip:HookScript('OnTooltipSetItem', gearscore_program);
+    AtlasLootTooltip:HookScript('OnTooltipSetItem', gearscore_program);
+    ItemRefTooltip:HookScript('OnTooltipSetItem', gearscore_program);
+    ShoppingTooltip1:HookScript('OnTooltipSetItem', gearscore_program);
+    ShoppingTooltip2:HookScript('OnTooltipSetItem', gearscore_program);
+end
---
+++ readme.md
@@ -0,0 +1,26 @@
+#FuryValue
+
+Un petit addon qui calcule l'apport de stats brutes DPS des objets (ici appelé "value") pour warfury 3.3.5a (ne prend pas en compte le niveau d'objet, ou l'endurance mais bien les stats dps uniquement).
+
+![Alt text](./screenshot.PNG "Title")
+
+Fonctionne aussi bien avec les tooltips "normales" que celles d'atlas-loot.
+
+Aucune configuration requise.
+
+Mes calculs prennent en compte pas mal de variables comme la bok, buffs en raids, talents (**Armé jusqu'aux dents 3/3 et Posture berserker améliorée 5/5**), etc... :
+
+**1 value = 1 score de pénétration d'armure = 1 score d'expertise = 1 au score de toucher = 2 au score de hâte = 64.8 armure = 0.69 force = 1.8 puissance d'attaque = 0.666 au score de crit = 0.815 agilité**
+
+Lorsque l'addon détecte des slots de gemmes, il va calculer plusieurs combinaisons à base de gemmes épiques rouges force, gemmes orange crit/force et gemmes vertes force/endu. L'addon retiendra la combinaison ayant le plus de "value" et vous indiquera s'il est rentable de gemmer en fonction du bonus de châsse ou non.
+
+Gardez bien à l'esprit que :
+
+1) Mes calculs sont 100% subjectifs. Par exemple, je part du principe que 1 toucher = 2 hâte. Pourquoi ? Parce que L'Oréal ! (Et aussi parce que la hâte plus t'en as et moins c'est pratique. Mais quand même, le 1 toucher = 2 hâte par exemple sort de mon cul ^^).
+
+2) Les scores affichés ne prennent pas en compte les différents caps. Si vous êtes capés toucher par exemple, vous préfèrez sûrement équiper une cape sans toucher à 250 value qu'une cape à 300 value avec toucher. Tout comme le gearscore, ce n'est qu'une indication, plus précise certes, mais une indication quand même.
+
+/!\ Testé uniquement sur client FR /!\
+
+Made by [Pseudo](https://way-of-elendil.fr/armory/character/779469-pseudo) pour [Pseudo](https://way-of-elendil.fr/armory/character/779469-pseudo).
+
